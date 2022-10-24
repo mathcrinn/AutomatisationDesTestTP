2a0ebd800c0a2e4de43ad34c274c0f386f215bf9
mathcrinn <mathis.crinchon@epsi.fr>
2022-10-24 11:31:57 +0200

    update Quality without cheese

---
+++ src/main/java/com/gildedrose/Shop.java
@@ -0,0 +1,14 @@
+package com.gildedrose;
+
+import java.util.ArrayList;
+
+public class Shop {
+	private ArrayList<Article> articles;
+
+	public Shop(){}
+	
+	public void updateQuality() {
+		
+	}
+	
+}
---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,16 @@
+package com.gildedrose;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GildedRoseTest {
+	
+    @Test
+    void should_build() {
+        assertTrue(true);
+    }
+
+}
+
+
